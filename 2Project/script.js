365029ac362576906d5af56dc83810df4fdbafc2
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-05 01:05:03 +0530

    code

--- 2Project/script.js
+++ 2Project/script.js
@@ -2,5 +2,10 @@
 
 // var rect = document.querySelector("#rect");
 window.addEventListener("mousemove",function(details){
-    
+
+    gsap.utils.
+    gsap.to('#rect',{
+        left: details.clientX + 'px',
+        ease: Power3
+    })
 })
