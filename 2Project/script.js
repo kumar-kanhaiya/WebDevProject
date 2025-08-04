7f88886242eefe24bc086abff89d4053a2c0befa
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-05 01:19:53 +0530

    code

--- 2Project/script.js
+++ 2Project/script.js
@@ -2,10 +2,14 @@
 
 // var rect = document.querySelector("#rect");
 window.addEventListener("mousemove",function(details){
+    var rect = this.document.querySelector("#rect");
 
-    gsap.utils.
+    let xval = gsap.utils.mapRange(0,this.window.innerWidth , 100 
+        + rect.getBoundingClientRect().width/2 , this.window.innerWidth - 100 
+        + (rect.getBoundingClientRect().width/2), details.clientX);
+
     gsap.to('#rect',{
-        left: details.clientX + 'px',
+        left: xval + 'px',
         ease: Power3
     })
 })
