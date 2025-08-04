8be41951a214d546ff03ad3b07abe3ae95fd15f1
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-04 21:58:13 +0530

    code

--- 1stProject/script.js
+++ 1stProject/script.js
@@ -3,7 +3,13 @@
 var rect = document.querySelector("#center");
 rect.addEventListener("mousemove",function(details){
     var rectangleLocation = rect.getBoundingClientRect();
-    var mousePosition = details.clientX - rectangleLocation.left;
-    console.log(mousePosition)
+    var insideRect = details.clientX - rectangleLocation.left;
+    let change = document.getElementById("center");
+    
+    if(insideRect < rectangleLocation.width/2){
+        let percentage = (rectangleLocation.width -insideRect)/100;
+        change.style.opacity = percentage;
+        change.style.backgroundColor = "red"
+    }
     
 })
