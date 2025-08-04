d63cd9193ed8b33be22807e18492a627740a724b
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-08-04 21:52:07 +0530

    code

--- 1stProject/script.js
+++ 1stProject/script.js
@@ -1,6 +1,9 @@
 // cursor position
 
 var rect = document.querySelector("#center");
-rect.addEventListener("mousemove",function(){
+rect.addEventListener("mousemove",function(details){
+    var rectangleLocation = rect.getBoundingClientRect();
+    var mousePosition = details.clientX - rectangleLocation.left;
+    console.log(mousePosition)
     
 })
