27100af4b98dfb092dbdee65dea895465cfd8ddf
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-16 21:26:41 +0530

    code

---
+++ MiniProject/src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import Card from './components/Card/card';
+ 
+
+  const App = () =>{
+  return (
+    <>
+      <Card logo="https://imgs.search.brave.com/FpnyRNRoovwDAwTfn4Un-es4uQ_6nByUIFalpA3R9EE/rs:fit:500:0:1:0/g:ce/aHR0cHM6Ly9zdDMu/ZGVwb3NpdHBob3Rv/cy5jb20vMTAwMTg2/MC8xNjM3NS9pLzQ1/MC9kZXBvc2l0cGhv/dG9zXzE2Mzc1NzYz/Mi1zdG9jay1waG90/by1hbWF6b24tbG9n/by1vbi1hLXdoaXRl/LmpwZw"
+      title="Amazon" 
+      dis="Apply for Amazon" 
+      date = "5"
+      tag1 = "Full Time"
+      tag2 = "Remote"
+      amount = "$120/hr"
+      add = "Mumbai" />
+      {/* <div className="card-wrapper">
+        <h1 style={{color: '#fff'}}>Hello World</h1>
+      </div> */}
+
+    </>
+  )
+}
+
+
+export default App
