623805acc8661cd5513978e17783225099aab5af
Kanhaiya Kumar <kanhaiyakumarmailme@gmail.com>
2025-10-23 13:51:57 +0530

    final code

--- Notes App/src/App.jsx
+++ Notes App/src/App.jsx
@@ -1,47 +1,90 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+
+  const [title, setTitle] = useState('')
+  const [details, setDetails] = useState('')
+
+  const [task, setTask] = useState([])
 
   const submitHandler = (e) => {
-    e.preventDefault();
+    e.preventDefault()
+
+    const copyTask = [...task];
+
+    copyTask.push({ title, details })
+
+    setTask(copyTask)
+
+    setTitle('')
+    setDetails('')
+  }
+
+
+  const deleteNote = (idx) => {
+    const copyTask = [...task];
+
+    copyTask.splice(idx, 1)
+
+    setTask(copyTask)
   }
 
   return (
-    <>
-    <div className='h-screen bg-black text-white '>
+    <div className='h-screen lg:flex bg-black text-white'>
 
-      <form action="" className=' flex flex-col gap-5 items-start  justify-between p-10 ' onSubmit={(e)=>{
-        submitHandler(e);
-      }}>
-        
-          <input 
-            type="text" 
-            
-            placeholder='Enter Notes Heading' 
-            className='border-2 w-full font-medium rounded py-2 m-2 px-5 outline-none '
+      <form onSubmit={(e) => {
+        submitHandler(e)
+      }} className='flex gap-4 lg:w-1/2 p-10 flex-col items-start'>
+
+        <h1 className='text-4xl mb-2 font-bold'>Add Notes</h1>
+
+        {/* PEHLA INPUT FOR HEADING */}
+        <input
+          type="text"
+          placeholder='Enter Notes Heading'
+          className='px-5 w-full font-medium py-2 border-2 outline-none rounded '
+          value={title}
+          onChange={(e) => {
+            setTitle(e.target.value)
+          }}
         />
+
+        {/* DETAILED VALA INPUT  */}
         <textarea
-            placeholder='Enter Details' 
-            className='border-2 w-full font-medium rounded py-2 px-5 h-32 flex items-start m-2 outline-none '
+          type="text"
+          className='px-5 w-full font-medium h-32 py-2 flex items-start flex-row border-2 outline-none  rounded '
+          placeholder='Write Details here'
+          value={details}
+          onChange={(e) => {
+            setDetails(e.target.value)
+          }}
         />
-        <button className='bg-white w-full text-black font-medium py-2 px-5 rounded outline-none' >Add Note</button>
-        
-        
-        {/* <img className='h-52' src='https://imgs.search.brave.com/-_fMBrgW105d5l-4b9vjAgNtkFj2NTYA82X85vePEKY/rs:fit:860:0:0:0/g:ce/aHR0cHM6Ly93d3cu/cG5nYWxsLmNvbS93/cC1jb250ZW50L3Vw/bG9hZHMvNS9ZZWxs/b3ctU3RpY2t5LU5v/dGUtUE5HLnBuZw' alt="" /> */}
-        </form>
-        <div className='flex flex-wrap p-10'>
 
-        <div className='h-32 w-32 rounded-2xl bg-white'></div>
+        <button
+          className='bg-white active:scale-95 font-medium w-full outline-none  text-black px-5 py-2 rounded'
+        >
+          Add Note
+        </button>
+
+      </form>
+      <div className='lg:w-1/2 lg:border-l-2  p-10'>
+        <h1 className='text-4xl font-bold'>Recent Notes</h1>
+        <div className='flex flex-wrap items-start justify-start gap-5 mt-6 h-[90%] overflow-auto'>
+          {task.map(function (elem, idx) {
+
+            return <div key={idx} className=" flex justify-between flex-col items-start relative h-52 w-40 bg-cover rounded-xl text-black pt-9 pb-4 px-4 bg-[url('https://static.vecteezy.com/system/resources/previews/037/152/677/non_2x/sticky-note-paper-background-free-png.png')]">
+              <div>
+                <h3 className='leading-tight text-lg font-bold'>{elem.title}</h3>
+                <p className='mt-2 leading-tight text-xs font-semibold text-gray-600'>{elem.details}</p>
+              </div>
+              <button onClick={() => {
+                deleteNote(idx)
+              }} className='w-full cursor-pointer active:scale-95 bg-red-500 py-1 text-xs rounded font-bold text-white'>Delete</button>
+            </div>
+          })}
         </div>
-
-
-        </div>
-
-    </>
+      </div>
+    </div>
   )
 }
 
